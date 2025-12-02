7d9afc80cebdf2a1e986a9ca1b368bd59d71eec6
AfonsoCMSousa <afonsoclerigomendessousa@gmail.com>
2025-12-02 21:46:21 +0000

    Update (2/12/2024)

--- 2025/include/file_io.cpp
+++ 2025/include/file_io.cpp
@@ -1,6 +1,7 @@
 #ifndef FILE_IO_CPP
 #define FILE_IO_CPP
 
+#include <cstdint>
 #include <fstream>
 #include <sstream>
 #include <string>
@@ -41,17 +42,67 @@ std::vector<std::vector<int>> read_int(const char *filePath)
     while (std::getline(file, line))
     {
         std::istringstream iss(line);
-        std::vector<int> intLine;
+        std::vector<int> longlongLine;
         int number;
         while (iss >> number)
         {
-            intLine.push_back(number);
+            longlongLine.push_back(number);
         }
-        lines.push_back(intLine);
+        lines.push_back(longlongLine);
     }
 
     file.close();
     return lines;
+}
+
+std::vector<std::vector<uint64_t>> read_int_seperator(const char *filePath, std::vector<char> seperators)
+{
+    std::ifstream file(filePath);
+    if (!file.is_open())
+    {
+        return std::vector<std::vector<uint64_t>>();
+    }
+
+    std::vector<std::vector<uint64_t>> lines;
+    std::string line;
+
+    while (std::getline(file, line))
+    {
+        std::vector<uint64_t> longlongLine;
+        std::string numberStr;
+        for (char ch : line)
+        {
+            if (std::find(seperators.begin(), seperators.end(), ch) != seperators.end())
+            {
+                if (!numberStr.empty())
+                {
+                    longlongLine.push_back(std::stoll(numberStr));
+                    numberStr.clear();
+                }
+            }
+            else
+            {
+                numberStr += ch;
+            }
+        }
+        if (!numberStr.empty())
+        {
+            longlongLine.push_back(std::stoll(numberStr));
+        }
+        lines.push_back(longlongLine);
+    }
+
+    file.close();
+    return lines;
+}
+
+std::vector<uint64_t> get_digits(uint64_t number) {
+    std::vector<uint64_t> _return_vec;
+	while (number != 0) {
+		_return_vec.push_back(number % 10);
+		number /= 10;
+	}
+	return _return_vec;
 }
 
 #endif // FILE_IO_CPP
