749d914ba792191a42abadec2bd864986c096b1a
AfonsoCMSousa <afonsoclerigomendesousa@gmail.com>
2025-12-09 17:58:10 +0000

    Updated (9/12/2025)

---
+++ 2025/Day4/main.cpp
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <cctype>
+#include <cstdint>
+#include <cstdio>
+#include <format>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <sys/types.h>
+#include <vector>
+
+#define __PART_2_SOLUTION 1
+const uint64_t max_digitis = 12;
+
+// Default FileIO file:
+#include "../include/file_io.cpp"
+
+using namespace std;
+
+int main(void) {
+	vector<vector<char>> lables = read_char("./test_case.txt");
+
+    return 0;
+}
