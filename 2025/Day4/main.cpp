cde8b60b9b34dd0dda0c488deb07623876226368
AfonsoCMSousa <afonsoclerigomendesousa@gmail.com>
2025-12-10 20:35:36 +0000

    Updated (10/12/2025)

--- 2025/Day4/main.cpp
+++ 2025/Day4/main.cpp
@@ -20,18 +20,21 @@ class Rool {
 	uint32_t x;
 	uint32_t y;
 	bool exists;
+	bool access;
 
   public:
 	Rool() {
 		x = 0;
 		y = 0;
 		this->exists = false;
+		this->access = false;
 	}
 
 	Rool(uint32_t x, uint32_t y) {
 		this->x = x;
 		this->y = y;
 		this->exists = true;
+		this->access = false;
 	}
 
 	uint32_t getX() {
@@ -45,6 +48,14 @@ class Rool {
 	bool isExists() {
 		return exists;
 	};
+
+	bool isAccess() {
+		return access;
+	}
+
+	void setAccess(bool access) {
+		this->access = access;
+	}
 };
 
 class Map {
@@ -53,45 +64,74 @@ class Map {
 	uint32_t __size;
 	uint32_t height;
 	uint32_t width;
+	uint32_t accessible_rools;
 
   public:
 	Map(uint32_t rows, uint32_t cols) {
-		this->__size = (rows * cols) + 1;
+		this->__size = (rows * cols);
 		this->height = rows;
 		this->width = cols;
-        map.resize(this->__size);
+		map.resize(this->__size);
 	};
 
 	void setRoolPos(uint32_t x, uint32_t y) {
 		if (x >= this->width || y >= this->height) {
 			throw runtime_error(format("Map is not big enought to enter ({},{}) in it", x, y));
 		}
-		map[x + (y * this->height)] = Rool(x, y);
+		map[y * this->width + x] = Rool(x, y);
 	};
 
+	void setRoolAccess(uint32_t x, uint32_t y) {
+		if (x >= this->width || y >= this->height) {
+			throw runtime_error(format("Map is not big enought to remove ({},{}) in it", x, y));
+		}
+
+		if (!map[y * this->width + x].isExists()) {
+			throw runtime_error(format("No Rool found at position ({},{}) to set access", x, y));
+		}
+
+		map[y * this->width + x].setAccess(true);
+		accessible_rools++;
+	};
+
+    void removeRoolPos(uint32_t x, uint32_t y) {
+        if (x >= this->width || y >= this->height) {
+            throw runtime_error(format("Map is not big enought to remove ({},{}) in it", x, y));
+        }
+        map[y * this->width + x] = Rool();
+    }
+
 	Rool getRoolPos(uint32_t x, uint32_t y) {
-		if ((x + (y * this->height))> __size) {
+		if ((x + (y * this->height)) > __size) {
 			throw runtime_error(format("Map is not big enought to get ({},{}) in it", x, y));
 		}
-		return map[x + (y * this->height)];
+		return map[y * this->width + x];
 	};
 
 	uint32_t size() {
 		return __size;
 	};
 
+	uint32_t getAccessibleRoolsCount() {
+		return accessible_rools;
+	}
+
 	// NOTE: Returns the number of rools nearby, like this:
 	// ...
 	// .@.
 	// @@.
 	// would return 2;
-	uint32_t getNearbyRools(uint32_t x, uint32_t y) {
+	uint32_t getNearbyRools(int32_t x, int32_t y) {
 		uint32_t count = 0;
 
-		for (int _y = y - 1; _y <= y + 1; _y++) {
-			if (_y < 0 || _y >= this->height)
-				continue;
+		if (!map[y * this->width + x].isExists()) {
+			return 0;
+		}
 
+		for (int _y = y - 1; _y <= y + 1; _y++) {
+			if (_y < 0 || _y >= this->height) {
+				continue;
+			}
 			for (int _x = x - 1; _x <= x + 1; _x++) {
 				if (_x < 0 || _x >= this->width)
 					continue;
@@ -108,24 +148,30 @@ class Map {
 		return count;
 	}
 
-    string toString() {
-        string result = "";
-        for (uint32_t y = 0; y < height; y++) {
-            for (uint32_t x = 0; x < width; x++) {
-                if (getRoolPos(x, y).isExists()) {
-                    result += "@";
-                } else {
-                    result += ".";
-                }
-            }
-            result += "\n";
-        }
-        return result;
-    }
+	string toString() {
+		string result = "";
+		for (uint32_t y = 0; y < height; y++) {
+			for (uint32_t x = 0; x < width; x++) {
+				if (getRoolPos(x, y).isAccess()) {
+					result += "x";
+				}
+
+				else if (getRoolPos(x, y).isExists()) {
+					result += "@";
+				}
+
+				else {
+					result += ".";
+				}
+			}
+			result += "\n";
+		}
+		return result;
+	}
 };
 
 int main(void) {
-	vector<vector<char>> map_string = read_char("./sample.txt");
+	vector<vector<char>> map_string = read_char("./test_case.txt");
 
 	Map map(map_string.size(), map_string[0].size());
 
@@ -147,9 +193,37 @@ int main(void) {
 		}
 	}
 
-    println("\nMap representation:\n{}", map.toString());
+#if __PART_2_SOLUTION == 1
+	while (1) {
+		uint32_t newly_accessible = 0;
+#endif
+		for (uint16_t line = 0; line < map_string.size(); line++) {
+			for (uint16_t letter = 0; letter < map_string[line].size(); letter++) {
+				if (map.getNearbyRools(letter, line) < 4 && map.getRoolPos(letter, line).isExists() && !map.getRoolPos(letter, line).isAccess()) {
+					map.setRoolAccess(letter, line);
+					if (__PART_2_SOLUTION == 1) 
+                        newly_accessible++;
+				}
+			}
+		}
 
-	println("Number of Rools arround (x:{}, y:{}) = ({}){}", 2, 0, map_string[0][2] ,map.getNearbyRools(2, 0));
+        for (uint16_t line = 0; line < map_string.size(); line++) {
+            for (uint16_t letter = 0; letter < map_string[line].size(); letter++) {
+                if (map.getRoolPos(letter, line).isAccess()) {
+                    map.removeRoolPos(letter, line);
+                }
+            }
+        }
+
+#if __PART_2_SOLUTION == 1
+		if (newly_accessible == 0) {
+			break;
+		}
+	}
+
+#endif
+	println("Map representation:\n{}", map.toString());
+	println("Accessible rools count: {}", map.getAccessibleRoolsCount());
 
 	return 0;
 }
